bbb4c6c3f62a46422c105e7474c30df8983e22fb
Andrew Skoryk <andrewtimelessone@gmail.com>
2023-10-23 14:40:29 +0300

    feat: tailwind cn utility function

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "./Provider";
+import { cn } from "./lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <Provider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={cn("antialiased", inter.className)}>{children}</body>
       </html>
     </Provider>
   );
